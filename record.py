eaae3b815a625db4bccea2c50b0c573493acf607
darrensyu <dasyu@ucsd.edu>
2015-07-18 23:32:47 -0700

    commented and functional recording feature; working towards presentable UI next; trim optimization tabled;

--- record.py
+++ record.py
@@ -14,14 +14,14 @@ THRESHOLD = 500
 
 
 
-def trim(snd_data):
+"""def trim(snd_data):
     "Trim the blank spots at the start and end"
     def _trim(snd_data):
         snd_started = False
-        r = array('h')
+        r = []
 
         for i in snd_data:
-            if not snd_started and abs(i)>THRESHOLD:
+            if not snd_started and i>THRESHOLD:
                 snd_started = True
                 r.append(i)
 
@@ -36,25 +36,36 @@ def trim(snd_data):
     snd_data.reverse()
     snd_data = _trim(snd_data)
     snd_data.reverse()
-    return snd_data
-
-def is_silent(snd_data):
-  return max(snd_data) < THRESHOLD
+    return snd_data"""
 
 p = pyaudio.PyAudio()
 
+# Dictionary of all words needed for the tonal dictonary
+# Structure of the Dictionary(key = word containing tone(s) 
+# value = array of tones for that word)
 wordsdict = {'tomorrow': ['to','mo','rrow'] , 
       'she': ['she'],
       'knee': ['knee'],
       'home':['ho','me'] }
 
-word = str(raw_input('Enter word to consider: '))
-word_arr = wordsdict[word]
-WORD_LENGTH = len(word_arr)
-#print WORD_LENGTH
-print ("The word is " + word)
-rec = raw_input("Speak the highlighted portion of the word when prompted.")
+# Debugging input line to allow developer to test specific words in tonal dictionary
+word = str(raw_input('Enter word to consider: ')).strip(" ")
 
+# Array of tones within the word being considered
+word_arr = wordsdict[word]
+
+# Number of tones within tone array
+WORD_LENGTH = len(word_arr)
+
+#print WORD_LENGTH
+
+# Inform user what word is to be shown for tonal recording
+print ("The word is " + word)
+
+# Instruction for user to only say the highlighted tone within the word
+rec = raw_input("Speak the capitalized portion of the word when prompted.")
+
+# Hitting "ENTER" begins the recording
 if (rec == ''):
   stream = p.open(format=FORMAT,
                 channels=CHANNELS,
@@ -63,28 +74,39 @@ if (rec == ''):
                 frames_per_buffer=CHUNK) #buffer
 
 frames = []
+#hframes = [h]
 
-for section in range(0,WORD_LENGTH):
+# For loop for each tone within the current word
+for tone in range(0,WORD_LENGTH):
+  # For loop for each recording segment of each tone
   for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
+    #hdata = [h,stream.read(CHUNK)]
     #print i
+
     if (i == 0):
       left = ''
+
+      # For loop highlighting the specific tone being recorded currently
+      # The tone in all caps is the tone being recorded
       for j in range(0,WORD_LENGTH):
-        if (j == section):
+        if (j == tone):
           left += word_arr[j].upper() + " "
         else:
           left += word_arr[j] + " "
       print left
     frames.append(data) # 2 bytes(16 bits) per CHANNELS
-  frames = trim(frames)
-  wf = wave.open(word_arr[section]+".wav",'wb')
+  #frames = trim(frames)
+
+  # Save the tonal recording as the tone_name.wav
+  wf = wave.open(word_arr[tone]+".wav",'wb')
   wf.setnchannels(CHANNELS)
   wf.setsampwidth(p.get_sample_size(FORMAT))
   wf.setframerate(RATE)
   wf.writeframes(b''.join(frames))
-
   wf.close()
+
+  # Reset the recording in preparation for the next tone
   frames = []
 
 
@@ -106,10 +128,10 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
-#wf = wave.open(WAVE_OUTPUT_FILENAME+"-"+word+".wav", 'wb')
+"""#wf = wave.open(WAVE_OUTPUT_FILENAME+"-"+word+".wav", 'wb')
 wf = wave.open(word+".wav",'wb')
 wf.setnchannels(CHANNELS)
 wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
-wf.close()
+wf.close()"""
